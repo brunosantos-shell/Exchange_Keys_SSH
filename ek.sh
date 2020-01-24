fc4e8cd7f997535c997122b22342cda80fc44d3e
brunosantos-shell <33733104+brunosantos-shell@users.noreply.github.com>
2020-01-23 21:42:27 -0300

    Update ek.sh
    Adicionando a porta de serviço como parâmetro do Script.

--- ek.sh
+++ ek.sh
@@ -6,12 +6,13 @@ ssh-keygen -t rsa -b 2048
 clear
 
 #Copiar Chave Pub para o Host remoto
-echo Digite o USUARIO@SERVER do host remoto
+echo "Digite o USUARIO@SERVER do host remoto"
 read remotehost
-#echo $remotehost
-ssh-copy-id $remotehost
+echo "Digite a porta do SSH"
+read porthost
+ssh-copy-id $remotehost -p $porthost
 
 #Checar Chave Pub no Host remoto
-ssh $remotehost cat  ~/.ssh/authorized_keys
+ssh -p $porthost $remotehost cat  ~/.ssh/authorized_keys
 
 exit
